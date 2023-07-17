69612610cd7aa25cf0d16a0ebb62f87aec988c32
Sejal-Khilari <86156431+Sejal-Khilari@users.noreply.github.com>
2023-07-17 15:56:08 +0530

    Update config.jsx

--- internship-management/src/config.jsx
+++ internship-management/src/config.jsx
@@ -1,4 +1,5 @@
 export default {
     // API_BASE_URL: "http://localhost:2324"
-    API_BASE_URL: "https://internshipmanagementbackend-production.up.railway.app"
+   // API_BASE_URL: "https://internshipmanagementbackend-production.up.railway.app"
+    API_BASE_URL:"https://internmanagementbackend-3onu.onrender.com"
 }
